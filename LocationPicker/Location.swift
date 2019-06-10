688ecd8bb7487afb80a83c756a19df8f83f329c6
iPastor <>
2019-06-10 15:41:19 +0300

    Location @objc

--- LocationPicker/Location.swift
+++ LocationPicker/Location.swift
@@ -12,15 +12,15 @@ import CoreLocation
 import AddressBookUI
 
 // class because protocol
-public class Location: NSObject {
-	public let name: String?
+@objc public class Location: NSObject {
+	@objc public let name: String?
 	
 	// difference from placemark location is that if location was reverse geocoded,
 	// then location point to user selected location
-	public let location: CLLocation
-	public let placemark: CLPlacemark
+	@objc public let location: CLLocation
+	@objc public let placemark: CLPlacemark
 	
-	public var address: String {
+	@objc public var address: String {
 		// try to build full address first
 		if let addressDic = placemark.addressDictionary {
 			if let lines = addressDic["FormattedAddressLines"] as? [String] {
@@ -34,7 +34,7 @@ public class Location: NSObject {
 		}
 	}
 	
-	public init(name: String?, location: CLLocation? = nil, placemark: CLPlacemark) {
+	@objc public init(name: String?, location: CLLocation? = nil, placemark: CLPlacemark) {
 		self.name = name
 		self.location = location ?? placemark.location!
 		self.placemark = placemark
@@ -48,7 +48,7 @@ extension Location: MKAnnotation {
 		return location.coordinate
 	}
 	
-    public var title: String? {
+    @objc public var title: String? {
 		return name ?? address
 	}
 }
